938830a18f482ce4d65bf645d9014c64e1f3d3c1
ozzzp <ozzzp@ozzzp-linux>
2013-09-14 20:34:32 +0800

    test IPP

--- tst/src/tst/main.java
+++ tst/src/tst/main.java
@@ -4,9 +4,9 @@ import java.*;
 public class tst
 {
    public static void main(String args[])
-   //向屏幕上写字符串"HelloWorld!"
+   向屏幕上写字符串"HelloWorld!"
     {
       System.out.print("HelloWorld!");
-      jhgfds;gjasheopfvghaes;vgashf
+      //jhgfds;gjasheopfvghaes;vgashf
     }
 }
