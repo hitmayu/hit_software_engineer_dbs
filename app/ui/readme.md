d3ceefc84b8cd7a9005af6e95579c78d1a75de4e
ozzzp <ozzzzzp@gmail.com>
2013-11-06 13:01:08 +0800

    init SQL webpage readme

---
+++ app/ui/readme.md
@@ -0,0 +1,18 @@
+网页文件夹说明:
+
+此文件夹的路径可用{{@UI}}设定, 参见setup.cfg
+
+css/	存放自制css的文件夹
+
+js/		存放自制js的文件夹
+
+images/	存放自制的图片
+
+lib/	存放bootstrap3 框架
+
+网页说明:
+index.html 	主页, 显示名称为{{@title}}, 显示是否登录, 若未登录, 显示登录连接; 若已登录, 显示姓名
+判定是否登录的变量是{{@login}}(true表示已登录), 若登录, 姓名的变量为{{@name}}
+
+signup.html 登录页面, 显示名称{{@title}}, 错误为{{@error_login}}, 如果登录失败, 则error_login=true.
+获取的用户名为uname, 密码为upasswd, (默认的)采用post方式.
---
+++ booking.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 06, 2013 at 11:27 AM
+-- Server version: 5.5.34
+-- PHP Version: 5.3.10-1ubuntu3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `booking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking_log`
+--
+
+CREATE TABLE IF NOT EXISTS `booking_log` (
+  `status` enum('agreed','coming','done','canceled') NOT NULL DEFAULT 'agreed',
+  `plan_id` int(10) NOT NULL,
+  `sid` int(10) NOT NULL,
+  `time_from` tinyint(4) NOT NULL,
+  `time_to` tinyint(4) NOT NULL,
+  PRIMARY KEY (`plan_id`),
+  KEY `sid` (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking_plan`
+--
+
+CREATE TABLE IF NOT EXISTS `booking_plan` (
+  `id` int(10) NOT NULL,
+  `tid` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `max_num` int(11) NOT NULL DEFAULT '1',
+  `time_from` tinyint(4) NOT NULL,
+  `time_to` tinyint(4) NOT NULL,
+  `type` enum('asked','free') NOT NULL DEFAULT 'free',
+  `note` text,
+  PRIMARY KEY (`id`),
+  KEY `tid` (`tid`),
+  KEY `data` (`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `defaults`
+--
+
+CREATE TABLE IF NOT EXISTS `defaults` (
+  `tid` int(10) NOT NULL,
+  `type` enum('free','asked') NOT NULL DEFAULT 'free',
+  `note` text,
+  `display_time_from` tinyint(4) NOT NULL DEFAULT '0',
+  `display_time_to` tinyint(4) NOT NULL DEFAULT '68',
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE IF NOT EXISTS `students` (
+  `sid` int(10) NOT NULL,
+  `phone` varchar(14) NOT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE IF NOT EXISTS `teacher` (
+  `tid` int(10) NOT NULL,
+  `phone` varchar(14) NOT NULL,
+  `location` varchar(40) NOT NULL,
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `passwd` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
