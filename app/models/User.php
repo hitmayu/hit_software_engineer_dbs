9b3f93e306177aad4a23495cd3b6e63b6d41e348
ozzzp <ozzzzzp@gmail.com>
2013-11-16 13:21:24 +0800

    Merge branch 'master' of https://github.com/hitmayu/hit_software_engineer_dbs into hitmayu-master
    Conflicts:
    	app/cfg/routes.cfg
    	app/ui/index.html

--- app/models/User.php
+++ app/models/User.php
@@ -20,11 +20,63 @@ class User{
 			return false;
 		}
 	}
+	static function exist($name, $value){
+		$r = F3::get('DB')->exec('SELECT * FROM user WHERE name = :uname ',
+			array( ':uname' => $value));
+		if(count($r) > 0)
+			return true;
+		else
+			return false;
+	}
+	static function signUp($info){
+		//var_dump($info);
+		if($info['pass'] !== $info['pass_check'])
+			return -1;
+		if(self::exist("name", $info['name']))
+			return -2;
+		//$uid = F3::get('DB')->lastinsertid();
+		$r = F3::get('DB')->exec("INSERT INTO user VALUES ('', :uname, :upass,:uusertype);",
+			array( ':uname' => $info['name'], ':upass' => $info['pass'],':uusertype'=>$info['type'])
+		);
+		$uid = F3::get('DB')->lastinsertid();//right?????
 
+	//	if($info['type'] == "teacher"){//teacher
+	//		$r = F3::get('DB')->exec("INSERT INTO teacher VALUES (:tid);",
+	//			array( ':tid' => $uid)
+	//		);
+	//	}else{// student
+		//	$r = F3::get('DB')->exec("INSERT INTO student VALUES (:sid);",
+		//		array(':sid' => $uid)
+		//	);
+			//Code::dump($r);
+		//}
+		//self::login(array('uid' => $uid, 'name' => $info['name']));
+		return $uid;
+	}
 	static function login($user){
-		setcookie('se_user_id', $user['id'], time() + 86400, '/');
-		setcookie('se_user_name', $user['name'], time() + 86400, '/');
-		setcookie('se_user_token', self::generate_login_token($user['id']), time() + 86400, '/');
+		//setcookie('se_user_id', $user['id'], time() + 86400, '/');
+		//setcookie('se_user_name', $user['name'], time() + 86400, '/');
+		//setcookie('se_user_type', $user['usertype'], time() + 86400, '/');
+		setcookie('se_user_realname', $user['realname'], time() + 86400, '/');	
+		setcookie('se_user_phone', $user['phone'], time() + 86400, '/');
+		setcookie('se_user_college', $user['college'], time() + 86400, '/');
+		if($user['usertype']=='teacher')
+		{
+			setcookie('se_user_id', $user['tid'], time() + 86400, '/');
+			setcookie('se_user_location', $user['location'], time() + 86400, '/');
+			setcookie('se_user_job', $user['job'], time() + 86400, '/');
+			setcookie('se_user_token', self::generate_login_token($user['tid']), time() + 86400, '/');
+
+			
+		}
+		else
+		{
+			setcookie('se_user_id', $user['sid'], time() + 86400, '/');
+			setcookie('se_user_specialty', $user['specialty'], time() + 86400, '/');
+			setcookie('se_user_token', self::generate_login_token($user['sid']), time() + 86400, '/');
+
+		}
+		//setcookie('se_user_token', self::generate_login_token($user['id']), time() + 86400, '/');
 
 		//Code::dump($user);
 	}
