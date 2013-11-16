c764768fa38f4a53276f649016ac5c0149d8b1be
ozzzp <ozzzzzp@gmail.com>
2013-11-16 14:18:25 +0800

    解决合并冲突问题, 更新数据库

--- booking.sql
+++ booking.sql
@@ -43,18 +43,19 @@ CREATE TABLE IF NOT EXISTS `booking_log` (
 --
 
 CREATE TABLE IF NOT EXISTS `booking_plan` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `tid` int(10) NOT NULL,
   `date` date NOT NULL,
   `max_num` int(11) NOT NULL DEFAULT '1',
   `time_from` tinyint(4) NOT NULL,
   `time_to` tinyint(4) NOT NULL,
   `type` enum('asked','free') NOT NULL DEFAULT 'free',
-  `note` text,
+  `address` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `note` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`),
   KEY `tid` (`tid`),
   KEY `data` (`date`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -77,11 +78,15 @@ CREATE TABLE IF NOT EXISTS `defaults` (
 -- Table structure for table `students`
 --
 
-CREATE TABLE IF NOT EXISTS `students` (
+CREATE TABLE IF NOT EXISTS `student` (
   `sid` int(10) NOT NULL,
   `phone` varchar(14) NOT NULL,
+  `college` varchar(40) NOT NULL,
+  `specialty` varchar(40) NOT NULL,
+  `realname` varchar(20) NOT NULL,
   PRIMARY KEY (`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 -- --------------------------------------------------------
 
@@ -92,7 +97,10 @@ CREATE TABLE IF NOT EXISTS `students` (
 CREATE TABLE IF NOT EXISTS `teacher` (
   `tid` int(10) NOT NULL,
   `phone` varchar(14) NOT NULL,
+  `college` varchar(40) NOT NULL,
   `location` varchar(40) NOT NULL,
+  `realname` varchar(20) NOT NULL,
+  `job` varchar(20) NOT NULL,
   PRIMARY KEY (`tid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -103,12 +111,13 @@ CREATE TABLE IF NOT EXISTS `teacher` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `passwd` varchar(20) NOT NULL,
   PRIMARY KEY (`id`),
+  `usertype` enum('teacher','student') NOT NULL,
   KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
